de7cb788c606cdeb1d385e37dbf873b1672279c9
pranjalv23 <pranjal200186@gmail.com>
2026-04-05 11:56:24 +0530

    updated the code

--- a2a_service/executor.py
+++ a2a_service/executor.py
@@ -5,7 +5,7 @@ from agents.agent import run_query
 
 logger = logging.getLogger("agent_research.a2a_executor")
 
-class ResearchExecutor(BaseAgentExecutor):
+class ResearchAgentExecutor(BaseAgentExecutor):
     """A2A executor that bridges incoming A2A tasks to the research agent."""
     def __init__(self):
         super().__init__(run_query_fn=run_query)
